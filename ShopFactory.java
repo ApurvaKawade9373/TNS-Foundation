ccaee54907f90f70984df260fdb46caad143701c
ApurvaKawade9373 <128119114+ApurvaKawade9373@users.noreply.github.com>
2024-01-25 14:54:46 +0530

    Delete ShopFactory.java

--- ShopFactory.java
+++
@@ -1,8 +0,0 @@
-package com.tnsif.framework;
-
-
-
-public abstract class ShopFactory {
-	public abstract PrimeAcc getNewPrimeAccount(int accNo, String accNm, float charges, boolean isPrime);
-	public abstract NormalAcc getNewNormalAccount(int accNo, String accNm, float charges, float deliveryCharges);
-}
